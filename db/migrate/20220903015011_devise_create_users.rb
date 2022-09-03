02f1eed164092127a3f4d39af1cd1910248552ef
kitten1139 <106716550+kitten1139@users.noreply.github.com>
2022-09-03 07:26:29 +0000

    [Add]userのマイグレーションファイルにカラム追加

--- db/migrate/20220903015011_devise_create_users.rb
+++ db/migrate/20220903015011_devise_create_users.rb
@@ -6,6 +6,13 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :nickname,           null: false
+      t.integer :gender,            null: false, default: 0
+      t.integer :age,               null: false
+      t.integer :prefecture,        null: false, default: 0
+      t.text :introduction,         null: false
+      t.string :favorite_food,      null: false
+      t.boolean :is_deleted,        null: false, default: "FALSE"
 
       ## Recoverable
       t.string   :reset_password_token
