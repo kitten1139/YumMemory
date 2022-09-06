1252ddcdd6020aa1ac23dd60f72f59c7f74b44a0
kitten1139 <106716550+kitten1139@users.noreply.github.com>
2022-09-06 08:44:10 +0000

    [Add]public側の会員情報編集画面と更新機能の追加

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,13 +1,30 @@
 class Public::UsersController < ApplicationController
-  
+before_action :authenticate_user!
+
   def show
+    @user = User.find(params[:id])
   end
-  
+
   def edit
+    @user = User.find(params[:id])
+    #編集するユーザーが本人でない場合はユーザーページにリダイレクトする
+    unless @user == current_user
+      redirect_to user_path(@user)
+    end
   end
-  
+
   def update
+    if current_user.update(user_params)
+      redirect_to user_path(current_user)
+    else
+      redirect_to edit_user_path(current_user)
+    end
   end
-    
+
+  private
+
+  def user_params
+    params.require(:user).permit(:nickname, :gender, :age, :prefecture, :introduction, :favorite_food, :profile_image)
+  end
 
 end
