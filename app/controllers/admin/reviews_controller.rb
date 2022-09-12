f117a67f7d22b7faa2345fe5ebcb09e4cd3b1c32
kitten1139 <106716550+kitten1139@users.noreply.github.com>
2022-09-12 07:29:03 +0000

    [Add]管理者側の会員レビュー詳細画面と削除機能

--- app/controllers/admin/reviews_controller.rb
+++ app/controllers/admin/reviews_controller.rb
@@ -1,5 +1,14 @@
 class Admin::ReviewsController < ApplicationController
   before_action :authenticate_admin!
+
+  def show
+    @post = Post.find(params[:id])
+  end
   
+  def destroy
+    post = Post.find(params[:id])
+    post.destroy
+    redirect_to admin_path
+  end
   
 end
--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -21,7 +21,7 @@ class Public::PostsController < ApplicationController
   end
 
   def index
-    @posts = Post.page(params[:page]).per(6)
+    @posts = Post.page(params[:page]).per(8)
     @total_posts = Post.count
   end
 
