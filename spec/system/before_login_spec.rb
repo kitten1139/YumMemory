a81f09089727b84e001d26d74d0422b4224127b2
kitten1139 <106716550+kitten1139@users.noreply.github.com>
2022-10-10 11:59:29 +0000

    [Add]新規登録のテストコード追加

--- spec/system/before_login_spec.rb
+++ spec/system/before_login_spec.rb
@@ -74,7 +74,7 @@ describe '[STEP1] ユーザログイン前のテスト' do
         guest_login_link.click
         is_expected.to eq '/posts'
       end
-      it '新規登録を押すと、新規登録画面に遷移する' do
+      it '新規登録を押すと、新規会員登録画面に遷移する' do
         signup_link = find_all('a')[3]
         signup_link.click
         is_expected.to eq '/users/sign_up'
@@ -83,6 +83,71 @@ describe '[STEP1] ユーザログイン前のテスト' do
         login_link = find_all('a')[4]
         login_link.click
         is_expected.to eq '/users/sign_in'
+      end
+    end
+  end
+
+  describe 'ユーザ新規登録のテスト' do
+    before do
+      visit new_user_registration_path
+    end
+
+    context '表示内容の確認' do
+      it 'URLが正しい' do
+        expect(current_path).to eq '/users/sign_up'
+      end
+      it '「新規会員登録」と表示される' do
+        expect(page).to have_content '新規会員登録'
+      end
+      it 'メールアドレスフォームが表示される' do
+        expect(page).to have_field 'user[email]'
+      end
+      it 'パスワードフォームが表示される' do
+        expect(page).to have_field 'user[password]'
+      end
+      it 'パスワード確認フォームが表示される' do
+        expect(page).to have_field 'user[password_confirmation]'
+      end
+      it 'プロフィール画像フォームが表示される' do
+        expect(page).to have_field 'user[image]'
+      end
+      it 'ニックネームフォームが表示される' do
+        expect(page).to have_field 'user[nickname]'
+      end
+      it '居住地フォームが表示される' do
+        expect(page).to have_field 'user[prefecture]'
+      end
+      it '性別フォームが表示される' do
+        expect(page).to have_field 'user[gender]'
+      end
+      it '年齢フォームが表示される' do
+        expect(page).to have_field 'user[age]'
+      end
+      it '好きな食べ物フォームが表示される' do
+        expect(page).to have_field 'user[favorite_food]'
+      end
+      it '自己紹介フォームが表示される' do
+        expect(page).to have_field 'user[introduction]'
+      end
+      it '新規登録ボタンが表示される' do
+        expect(page).to have_button '新規登録'
+      end
+    end
+
+    context '新規登録成功のテスト' do
+      before do
+        fill_in 'user[email]', with: Faker::Internet.email
+        fill_in 'user[password]', with: 'password'
+        fill_in 'user[password_confirmation]', with: 'password'
+        fill_in 'user[nickname]', with: Faker::Lorem.characters(number: 10)
+      end
+
+      it '正しく新規登録される' do
+        expect { click_button '新規登録' }.to change(User.all, :count).by(1)
+      end
+      it '新規登録後のリダイレクト先が、新規登録できたユーザの詳細画面になっている' do
+        click_button '新規登録'
+        expect(current_path).to eq '/users/' + User.last.id.to_s
       end
     end
   end
