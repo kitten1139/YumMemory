123ed0567eacebc5a03e5b6723927ea2ee39c7ec
kitten1139 <106716550+kitten1139@users.noreply.github.com>
2022-10-13 01:29:47 +0000

    [Add]ログイン後のヘッダーリンク先のテストコードを追加

--- spec/system/after_login_spec.rb
+++ spec/system/after_login_spec.rb
@@ -1,11 +1,52 @@
 require 'rails_helper'
 
-describe '投稿のテスト' do
+describe '[STEP2] ユーザログイン後のテスト' do
+  let(:user) { create(:user) }
+  let!(:other_user) { create(:user) }
+  let!(:post) { create(:post, user: user) }
+  let!(:other_post) { create(:post, user: other_user) }
+
+  before do
+    visit new_user_session_path
+    fill_in 'user[email]', with: user.email
+    fill_in 'user[password]', with: user.password
+    click_button 'ログイン'
+  end
+
+  describe 'ヘッダーのテスト: ログインしている場合' do
+    context 'リンクの内容を確認: ※logoutは『ユーザログアウトのテスト』でテスト済み' do
+      subject { current_path }
+
+      it 'サイトロゴを押すと、トップ画面に遷移する' do
+        home_link = find_all('a')[0]
+        home_link.click
+        is_expected.to eq '/'
+      end
+      it 'マイページを押すと、マイページ画面に遷移する' do
+        mypage_link = find_all('a')[1]
+        mypage_link.click
+        is_expected.to eq '/users/' + user.id.to_s
+      end
+      it 'みんなの投稿一覧を押すと、みんなの投稿一覧画面に遷移する' do
+        posts_link = find_all('a')[2]
+        posts_link.click
+        is_expected.to eq '/posts'
+      end
+      it 'お気に入りを押すと、マイお気に入り一覧画面に遷移する' do
+        myfavorite_link = find_all('a')[3]
+        myfavorite_link.click
+        is_expected.to eq '/users/' + user.id.to_s + '/my_favorites'
+      end
+      it 'カテゴリ検索するを押すと、カテゴリ一覧画面に遷移する' do
+        categories_link = find_all('a')[4]
+        categories_link.click
+        is_expected.to eq '/categories'
+      end
+    end
+  end
+
   describe "新規投稿画面(new_post_path)のテスト" do
     before do
-      @post = FactoryBot.create(:post)
-      @user = FactoryBot.create(:user)
-      sign_in @user
       visit new_post_path
     end
 
@@ -28,5 +69,4 @@ describe '投稿のテスト' do
       end
     end
   end
-
 end
